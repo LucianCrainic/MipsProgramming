432c19153f187db70ae299109a59472f7b713973
Lucian D. Crainic <crainic.lucian@gmail.com>
2022-10-09 08:50:31 +0200

    Add Chapter 4 exercises text

--- Chapter4/ex_1.asm
+++ Chapter4/ex_1.asm
@@ -1,3 +1,18 @@
+# ----> ITALIAN <----
+# Leggere 7 valori interi da input e calcolarne 
+# la media (stampandola a video).
+# INPUT
+# 12;
+# 82;
+# 11;
+# 2345;
+# 67;
+# 123456;
+# 675
+# OUTPUT
+# 126069.428571428
+
+NB: La media deve essere espressa con un numero reale (float). Utilizzare il coprocessore matematico.
 .text
 .globl main
 main:	
--- Chapter4/ex_2.asm
+++ Chapter4/ex_2.asm
@@ -1,3 +1,12 @@
+# ----> ITALIAN <----
+# Effettuare la sommatoria di numeri reali poistivi 
+# immessi da input. La sommatoria è calcolata quando 
+# il valore immesso dall'utente è nullo o negativo.
+# INPUT
+# 3.5;7.23;5.6;9.17;-1
+# OUTPUT
+# 25.5
+
 .text
 .globl main
 main:
